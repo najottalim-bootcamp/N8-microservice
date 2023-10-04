1804f3fe22878a76cf4a0b458ff5683b29693d93
abdisamadovich <islombobokulov143@gmail.com>
2023-10-04 12:23:41 +0500

    Islombek Boboqulov

---
+++ Islombek/Controllers/islombekController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Islombek.Controllers
+{
+    [Route("api/islombek")]
+    [ApiController]
+    public class islombekController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Islombek", "Boboqulov" };
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return id+" Islombek Boboqulov";
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
