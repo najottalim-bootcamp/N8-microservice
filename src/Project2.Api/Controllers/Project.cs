5e2f90f662cd9ebacd4973495714d629e9f7dbce
Mirfayz <muiilhom@gmail.com>
2023-10-04 12:26:31 +0500

    Added

---
+++ src/Project2.Api/Controllers/Project.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project2.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Project : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get() 
+            => new string[] { "value1", "value2" };
+
+        [HttpGet("{id}")]
+        public  string Get(int id)
+            => $"Bu siz kiritgan raqam + {id}";
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+            => Ok(true);
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+            => Ok(true);
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+            => Ok(true);
+    }
+}
